7811d50b45687137eb38aa1225da263ab2be1509
ksgy <ksgy@x-plane.hu>
2014-09-26 13:52:25 +0100

    admin views

---
+++ app/src/admin/controllers/admin.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('euroscopeEyeApp')
+	.controller('AdminAdminCtrl', function ($scope) {
+
+	});
--- app/src/app.js
+++ app/src/app.js
@@ -1,20 +1,24 @@
 'use strict';
 
 angular.module('euroscopeEyeApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute',
-  'google-maps',
-  'n3-line-chart'
+	'ngCookies',
+	'ngResource',
+	'ngSanitize',
+	'ngRoute',
+	'google-maps',
+	'n3-line-chart'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'src/common/views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'src/common/views/main.html',
+				controller: 'MainCtrl'
+			})
+			.when('/admin', {
+				templateUrl: 'src/admin/views/admin.html',
+				controller: 'AdminAdminCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
